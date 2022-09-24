e2546cce80a74edf6091fb458ed93786dcaf9fb6
Sanskar <thakursanskaranhal@gmail.com>
2022-09-24 20:12:30 +0530

    aded a container

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animation',
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Color.fromARGB(200, 23, 255, 255),
+            child: Text(
+              'Animation',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
