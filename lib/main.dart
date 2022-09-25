db920652d560357d4a3ae611e5e1b6320ac2d06a
Sanskar <thakursanskaranhal@gmail.com>
2022-09-25 19:50:21 +0530

    created starting animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,55 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  double width = 200;
+  double height = 200;
+
+  void updateState() {
+    setState(() {
+      width = 400;
+      height = 400;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Animation',
       home: Scaffold(
         body: Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Color.fromARGB(200, 23, 255, 255),
-            child: Text(
-              'Animation',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 400),
+                  width: width,
+                  height: height,
+                  color: const Color.fromARGB(200, 23, 255, 255),
+                  child: Center(
+                    child: Text(
+                      'Animation',
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    updateState();
+                  },
+                  child: const Text('Animate'),
+                )
+              ]),
         ),
       ),
     );
